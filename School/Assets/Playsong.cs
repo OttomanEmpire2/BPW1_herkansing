1b92496c457c3516c7e2d091674eb7ed56d21521
OttomanEmpire2 <jvd_b@hotmail.nl>
2019-06-17 18:54:51 +0200

    The game

---
+++ School/Assets/Playsong.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsong : Target
+{
+    public AudioSource backgroundAudio;
+    public AudioClip newBackgroundSong;
+
+    public override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+        ChangeBackgroundSound();
+    }
+
+    public void ChangeBackgroundSound()
+    {
+        backgroundAudio.clip = newBackgroundSong;
+        backgroundAudio.Play();
+    }
+
+}
